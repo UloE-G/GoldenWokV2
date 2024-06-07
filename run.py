de408db41fe838f5c67ee4d9f356d35fc2d6b0a8
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-06-07 13:19:04 +0000

    Created menu route

--- run.py
+++ run.py
@@ -10,19 +10,14 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
 
 
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
-
-
-@app.route("/careers")
-def careers():
-    return render_template("careers.html")
 
 
 if __name__ == "__main__":
